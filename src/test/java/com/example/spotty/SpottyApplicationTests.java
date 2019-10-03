6706b70604b96c763bb463d9dd7885c09b9d3e39
ggatling <gerda.gatling@gmail.com>
2019-10-03 19:53:34 -0400

    First commit

---
+++ src/test/java/com/example/spotty/SpottyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spotty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpottyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
