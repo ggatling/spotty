f22659c216bc03df79dc44de8ba28f30f92bdf67
ggatling <gerda.gatling@gmail.com>
2019-10-03 20:33:34 -0400

    Added UserRepository

---
+++ src/main/java/com/example/spotty/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.spotty.repository;
+
+import com.example.spotty.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+}
