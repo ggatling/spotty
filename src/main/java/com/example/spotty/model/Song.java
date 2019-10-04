a1a640dba9acdb9b8e8b4aeb940741b14f81dbda
ggatling <gerda.gatling@gmail.com>
2019-10-03 20:28:21 -0400

    Added getters and setters for the User and Song models

--- src/main/java/com/example/spotty/model/Song.java
+++ src/main/java/com/example/spotty/model/Song.java
@@ -1,4 +1,52 @@
 package com.example.spotty.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Song {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String title;
+
+    private String artist;
+
+    private double length;
+
+    public Song(){}
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
 }
